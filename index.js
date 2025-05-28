f7f141c702258d4b822fb308c3554b0ef8546168
Abusolay <sefat01625@gmail.com>
2025-05-28 23:54:51 +0600

    jobs id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express(); 
 const port = process.env.PORT || 3000;
@@ -34,6 +34,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/jobs/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
